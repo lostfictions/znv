3f1ac08521ef047de95d475d461a275a2396b58e
David Heidrich <me@bowlingx.com>
2025-03-20 21:46:50 +0100

    fix: fix type exports, run jobs on all pr's

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ export { z } from "zod";
 export * from "./parse-env.js";
 export * from "./preprocessors.js";
 export * from "./extra-schemas.js";
-export type * from "./util.js";
+export type {
+  DeepReadonly,
+  DeepReadonlyArray,
+  DeepReadonlyObject,
+} from "./util.js";
