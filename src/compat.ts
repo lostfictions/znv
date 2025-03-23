87f7a01c371760318e16c949c7acb25ad771465f
lostfictions <lostfictions@users.noreply.github.com>
2025-03-22 20:20:59 -0400

    re-export type helpers to compat entrypoint too

--- src/compat.ts
+++ src/compat.ts
@@ -2,6 +2,11 @@ export { z } from "zod";
 export * from "./parse-env.js";
 export * from "./preprocessors.js";
 export * from "./extra-schemas.js";
+export type {
+  DeepReadonly,
+  DeepReadonlyArray,
+  DeepReadonlyObject,
+} from "./util/type-helpers.js";
 
 import { parseEnvImpl, type ParseEnv } from "./parse-env.js";
 
